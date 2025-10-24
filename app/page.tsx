a7833b43c35358328146d6bd6d563a2037ef95bc
Joelheile <joel.heil-escobar@outlook.de>
2025-10-24 08:51:13 +0200

    fix: redirect asking for prompt instead of just switching page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useRouter } from "next/navigation";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  redirect("/dashboard");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/dashboard");
+  }, [router]);
+
+  return null;
 }
