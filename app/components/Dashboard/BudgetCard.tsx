6f850ce7d3ac5e030b6db6c99c1f17216ac7095b
Joelheile <joel.heil-escobar@outlook.de>
2025-10-29 14:46:37 +0100

    feat: simplified card as change is unnecessary for this moment

--- app/components/Dashboard/BudgetCard.tsx
+++ app/components/Dashboard/BudgetCard.tsx
@@ -1,34 +1,36 @@
-import { TrendingDown, TrendingUp } from "lucide-react";
 import { Card } from "../ui/card";
+
+
 
 interface BudgetCardTypes {
   title: string;
   amount: number;
   changePercent?: number;
+  description?: string;
 }
 
-const BudgetCard = ({ title, amount, changePercent }: BudgetCardTypes) => {
-  const formattedAmount = `${amount.toLocaleString("de-DE")}€`;
-  const isPositive = changePercent !== undefined ? changePercent >= 0 : true;
+const BudgetCard = ({
+  title,
+  amount,
+  changePercent,
+  description,
+}: BudgetCardTypes) => {
+  const formattedAmount = `${amount.toLocaleString("de-DE", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })} €`;
 
   return (
     <Card className="w-full p-3">
-      <div className="flex justify-between items-center">
-        <h3 className="text-base font-semibold">{title}</h3>
-        {changePercent !== undefined && (
-          <div className="px-2  border rounded-md flex items-center gap-1">
-            {isPositive ? (
-              <TrendingUp className="w-3 h-3 text-muted-foreground" />
-            ) : (
-              <TrendingDown className="w-3 h-3 text-muted-foreground" />
-            )}
-            <span className="text-xs font-medium text-muted-foreground">
-              {Math.abs(changePercent)}%
-            </span>
-          </div>
-        )}
+      <div className="flex justify-between items-start">
+        <div className="flex flex-col">
+          <h3 className="text-base font-semibold">{title}</h3>
+          {description && (
+            <p className="text-xs text-muted-foreground mt-1">{description}</p>
+          )}
+        </div>
       </div>
-      <div className="">
+      <div className="mt-2">
         <span className="text-3xl font-semibold">{formattedAmount}</span>
       </div>
     </Card>
