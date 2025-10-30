33a84a778f3c944cf42f6b49846babccefabf6da
Joelheile <joel.heil-escobar@outlook.de>
2025-10-30 12:01:34 +0100

    chore: merged auth user

---
+++ convex/queries/users/getCurrentUser.ts
@@ -0,0 +1,12 @@
+import { getAuthUserId } from "@convex-dev/auth/server";
+import { Doc } from "../../_generated/dataModel";
+import { MutationCtx, QueryCtx } from "../../_generated/server";
+
+
+export async function getCurrentUser(ctx: QueryCtx | MutationCtx) {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return null;
+    const user = await ctx.db.get(userId);
+    if (!user) return null;
+    return user as Doc<"users"> & { organizationId: NonNullable<Doc<"users">["organizationId"]> };
+  }
