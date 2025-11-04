60b634fbe3b159cb14cd34ad0ee5df61d87d7d2f
Joelheile <joel.heil-escobar@outlook.de>
2025-11-04 15:36:56 +0100

    feat: implemented user query

--- app/components/Sidebar/AppSidebar.tsx
+++ app/components/Sidebar/AppSidebar.tsx
@@ -7,7 +7,6 @@ import {
   Upload,
   Users,
 } from "lucide-react";
-import type * as React from "react";
 import { memo } from "react";
 
 import {
@@ -25,43 +24,18 @@ import { api } from "../../../convex/_generated/api";
 import { MainNav } from "./MainNav";
 import { ProjectNav } from "./ProjectNav";
 import { SearchForm } from "./SearchForm";
+import { NavUser } from "./UserNav";
 
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    // avatar: "/avatars/shadcn.jpg",
-  },
-  mainNav: [
-    {
-      name: "Dashboard",
-      url: "/dashboard",
-      icon: LayoutDashboard,
-    },
-    {
-      name: "Transaktionen",
-      url: "/transactions",
-      icon: SquareCheckBig,
-    },
-    {
-      name: "Import",
-      url: "/import",
-      icon: Upload,
-    },
-    {
-      name: "Förderer",
-      url: "/donors",
-      icon: Users,
-    },
-  ],
-};
+const mainNav = [
+  { name: "Dashboard", url: "/dashboard", icon: LayoutDashboard },
+  { name: "Transaktionen", url: "/transactions", icon: SquareCheckBig },
+  { name: "Import", url: "/import", icon: Upload },
+  { name: "Förderer", url: "/donors", icon: Users },
+];
 
-function AppSidebarComponent({
-  ...props
-}: React.ComponentProps<typeof Sidebar>) {
-  const organizationName = useQuery(
-    api.organizations.queries.getOrganizationName,
-  );
+function AppSidebarComponent(props: React.ComponentProps<typeof Sidebar>) {
+  const user = useQuery(api.users.queries.getCurrentUserProfile);
+
   return (
     <Sidebar variant="sidebar" {...props}>
       <SidebarHeader>
@@ -82,10 +56,12 @@ function AppSidebarComponent({
         <SearchForm />
       </SidebarHeader>
       <SidebarContent>
-        <MainNav mainNav={data.mainNav} />
+        <MainNav mainNav={mainNav} />
         <ProjectNav />
       </SidebarContent>
-      <SidebarFooter>{/* <NavUser user={data.user} /> */}</SidebarFooter>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
     </Sidebar>
   );
 }
