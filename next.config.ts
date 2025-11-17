aa538ca0aea6dc2bf6552156fe7f20bc31f2e950
Joelheile <joel.heil-escobar@outlook.de>
2025-11-17 08:51:54 +0100

    feat: installed posthog

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
