8ee2f3dd33f2314c568447dd76a4d8f60eba28d2
Joelheile <joel.heil-escobar@outlook.de>
2025-12-08 19:53:12 +0100

    fix: got integration test module finally working

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "edge-runtime",
     server: { deps: { inline: ["convex-test"] } },
+    projects: [
+      {
+        test: {
+          include: ["convex/**"],
+          environment: "edge-runtime",
+        },
+      },
+    ],
   },
 });
