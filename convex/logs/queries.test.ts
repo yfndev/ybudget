b3341f962d7838be6745fcc21a2e2b4f62c760dd
Joelheile <joel.heil-escobar@outlook.de>
2025-12-10 10:49:55 +0100

    chore: simplified logs and adapted to variable conventions

--- convex/logs/queries.test.ts
+++ convex/logs/queries.test.ts
@@ -5,20 +5,21 @@ import schema from "../schema";
 import { modules, setupTestData } from "../test.setup";
 
 test("get logs", async () => {
-  const test = convexTest(schema, modules);
-  const { organizationId, userId } = await setupTestData(test);
+  const t = convexTest(schema, modules);
+  const { organizationId, userId } = await setupTestData(t);
 
-  await test.run(async (ctx) => {
-    await ctx.db.insert("logs", {
+  await t.run((ctx) =>
+    ctx.db.insert("logs", {
       organizationId,
       userId,
       action: "testAction",
       entityId: "testEntityId",
-    });
-  });
+    }),
+  );
 
-  const user = test.withIdentity({ subject: userId });
-  const logs = await user.query(api.logs.queries.getLogs, {});
+  const logs = await t
+    .withIdentity({ subject: userId })
+    .query(api.logs.queries.getLogs, {});
+
   expect(logs).toHaveLength(1);
-  expect(logs[0].action).toBe("testAction");
 });
