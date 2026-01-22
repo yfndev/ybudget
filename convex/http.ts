be91e28bdc9633d00499a798279ab352df62b8fd
Joelheile <joel.heil-escobar@outlook.de>
2026-01-22 02:01:19 +0100

    chore: remove Stripe integration

--- convex/http.ts
+++ convex/http.ts
@@ -1,25 +1,8 @@
 import { httpRouter } from "convex/server";
-import { internal } from "./_generated/api";
-import { httpAction } from "./_generated/server";
 import { auth } from "./auth";
 
 const http = httpRouter();
 
 auth.addHttpRoutes(http);
-
-http.route({
-  path: "/stripe",
-  method: "POST",
-  handler: httpAction(async (ctx, request) => {
-    const signature = request.headers.get("stripe-signature") as string;
-    const result = await ctx.runAction(internal.stripe.fulfill, {
-      signature,
-      payload: await request.text(),
-    });
-    return new Response(result.success ? null : "Webhook Error", {
-      status: result.success ? 200 : 400,
-    });
-  }),
-});
 
 export default http;
